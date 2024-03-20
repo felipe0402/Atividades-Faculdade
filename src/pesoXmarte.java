64f54d3a239f73d981bf2d8b00b7ce5e1775d1b5
felipe0402 <felipebaiano04@gmail.com>
2024-03-20 00:54:39 -0300

    Add atividade de ver meu peso em marte/lua

---
+++ src/pesoXmarte.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class pesoXmarte {
+
+public static void main(String[] args){
+Scanner s = new Scanner(System.in);
+
+System.out.println("");
+System.out.println("1 Marte ");
+System.out.println("2 Lua ");
+System.out.println("Opçao: ");
+
+int opcao = s.nextInt();
+
+if (opcao == 1) {
+  System.out.println("Digite seu peso:  ");
+  double peso = s.nextDouble();
+  System.out.println("Seu peso  em Marte sera: " + (peso / 9.81) * 3.6);
+} 
+else if(opcao == 2){
+System.out.println("Digite seu peso:  ");
+double peso = s.nextDouble();
+System.out.println("Seu peso  em Marte sera: " + (peso / 9.81) * 1.6);
+}else{
+  System.out.println("ERROR tente novamente");
+}
+
+}
+}
