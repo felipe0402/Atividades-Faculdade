f065354adb5a95e8096e831cef5ebdc4ceb5dece
felipe0402 <felipebaiano04@gmail.com>
2024-03-25 23:12:11 -0300

    Atualizando atividade switch case

--- src/switch_case_atvd_01.java
+++ src/switch_case_atvd_01.java
@@ -11,34 +11,84 @@ public class switch_case_atvd_01 {
       System.out.println("Didite o Ano:");
       int ano = s.nextInt();
 
+    
+
 
    switch (mes) {
-    case 1: System.out.println(dia + " Janeiro " + ano);
+    case 1: 
+    System.out.println("");   
+    System.out.println(dia +"/"+ mes +"/"+ ano);
+    System.out.println(dia + " de Janeiro de " + ano);
+    System.out.println("");   
     break;
-    case 2: System.out.println(dia + " Fevereiro " + ano);
+    case 2: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Fevereiro de " + ano);
+    System.out.println("");   
     break;
-    case 3: System.out.println(dia + " Março " + ano);
+    case 3: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Março de " + ano);
+    System.out.println("");   
     break;
-    case 4: System.out.println(dia + " Abril  " + ano);
+    case 4: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Abril de " + ano);
+    System.out.println("");   
     break;
-    case 5: System.out.println(dia + " Maio " + ano);
+    case 5: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Maio de " + ano);
+    System.out.println("");   
     break;
-    case 6: System.out.println(dia + " Junho " + ano);
+    case 6: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Junho de " + ano);
+    System.out.println("");   
     break;
-    case 7: System.out.println(dia + " Julho " + ano);
+    case 7: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Julho de " + ano);
+    System.out.println("");   
     break;
-    case 8: System.out.println(dia + " Agosto " + ano);
+    case 8: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Agosto de " + ano);
+    System.out.println("");   
     break;
-    case 9: System.out.println(dia + " Setembro " + ano);
+    case 9: 
+    System.out.println("");   
+    System.out.println(dia + "/0" + mes + "/"+ ano);
+    System.out.println(dia + " de Setembro de " + ano);
+    System.out.println("");   
     break;
-    case 10: System.out.println(dia + " Outubro " + ano);
+    case 10:
+         System.out.println(" ");
+         System.out.println(dia + "/" + mes + "/"+ ano);
+         System.out.println(dia + " de Outubro de " + ano);
+         System.out.println("");   
     break;
-    case 11: System.out.println(dia + " Novembro " + ano);
+    case 11: 
+        System.out.println(" ");
+        System.out.println(dia + "/" + mes + "/"+ ano);
+        System.out.println(dia + " de Novembro de " + ano);
+        System.out.println("");   
     break;
-    case 12: System.out.println(dia + " Desembro " + ano);
+    case 12:
+        System.out.println(" ");
+        System.out.println(dia + "/" + mes + "/"+ ano);
+        System.out.println(dia + " de Desembro de " + ano);
+        System.out.println("");   
     break;
    
-    default: System.out.println("Dados incorretos ");
+    default: System.out.print("Dados incorretos ");
       break;
    }
 
